9279ce263ba52cb03029c50c8ea0ad4d5199a383
kangseungmin <rkdtmdals999@naver.com>
2024-01-23 17:18:39 +0900

    [feat] Post 레코드 view, list, delete API 메소드 구현

--- src/main/java/com/example/simpleboard/post/controller/PostApiController.java
+++ src/main/java/com/example/simpleboard/post/controller/PostApiController.java
@@ -2,9 +2,12 @@ package com.example.simpleboard.post.controller;
 
 import com.example.simpleboard.post.db.PostEntity;
 import com.example.simpleboard.post.model.PostRequest;
+import com.example.simpleboard.post.model.PostViewRequest;
 import com.example.simpleboard.post.service.PostService;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,27 @@ public class PostApiController {
             @Valid @RequestBody PostRequest postRequest
     ) {
         return postService.create(postRequest);
+    }
+
+    @PostMapping("/view")
+    public PostEntity view(
+            @Valid @RequestBody PostViewRequest postViewRequest
+    ) {
+        return postService.view(postViewRequest);
+    }
+
+
+    @GetMapping("/all")
+    public List<PostEntity> list() {
+        return postService.all();
+
+    }
+
+
+    @PostMapping("/delete")
+    public void delete(
+            @Valid @RequestBody PostViewRequest postViewRequest
+    ) {
+        postService.delete(postViewRequest);
     }
 }
--- src/main/java/com/example/simpleboard/post/service/PostService.java
+++ src/main/java/com/example/simpleboard/post/service/PostService.java
@@ -3,7 +3,9 @@ package com.example.simpleboard.post.service;
 import com.example.simpleboard.post.db.PostEntity;
 import com.example.simpleboard.post.db.PostRepository;
 import com.example.simpleboard.post.model.PostRequest;
+import com.example.simpleboard.post.model.PostViewRequest;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +28,49 @@ public class PostService {
                 .build();
 
         return postRepository.save(entity);
+    }
+
+    /** 확인해야 할 사항
+     * 1. 게시글이 있는가?
+     * 2. 비밀번호가 맞는가?
+    * */
+    public PostEntity view(PostViewRequest postViewRequest) {
+
+        return postRepository.findFirstByIdAndStatusOrderByIdDesc(postViewRequest.getPostId(), "REGISTERED")
+                .map(it -> {
+                    if (!it.getPassword().equals(postViewRequest.getPassword())) {
+                        String format = "패스워드가 틀렸습니다. (입력받은값: %s, 실제비밀번호: %s)";
+                        throw new RuntimeException(String.format(format, postViewRequest.getPassword(), it.getPassword()));
+                    }
+                    return it;
+                })
+                .orElseThrow(
+                        () -> {
+                            return new RuntimeException("해당 게시글이 존재하지 않습니다: " + postViewRequest.getPostId());
+                        }
+                );
+    }
+
+    public List<PostEntity> all() {
+        return postRepository.findAll();
+    }
+
+    public void delete(PostViewRequest postViewRequest) {
+        postRepository.findById(postViewRequest.getPostId())
+                .map(it -> {
+                    if (!it.getPassword().equals(postViewRequest.getPassword())) {
+                        String format = "패스워드가 틀렸습니다. (입력받은값: %s, 실제비밀번호: %s)";
+                        throw new RuntimeException(String.format(format, postViewRequest.getPassword(), it.getPassword()));
+                    }
+
+                    it.setStatus("UNREGISTERED");
+                    postRepository.save(it);
+                    return it;
+                })
+                .orElseThrow(
+                        () -> {
+                            return new RuntimeException("해당 게시글이 존재하지 않습니다: " + postViewRequest.getPostId());
+                        }
+                );
     }
 }
