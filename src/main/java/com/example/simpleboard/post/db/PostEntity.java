2aecf00536a90f6de39ecb6c3b6b7301e2cb0791
kangseungmin <rkdtmdals999@naver.com>
2024-01-23 16:55:35 +0900

    [feat] board, post table의 controller, db, model, service 구현

---
+++ src/main/java/com/example/simpleboard/post/db/PostEntity.java
@@ -0,0 +1,33 @@
+package com.example.simpleboard.post.db;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "post")
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long boardId;
+    private String userName;
+    private String password;
+    private String email;
+    private String status;
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private LocalDateTime postedAt;
+}
